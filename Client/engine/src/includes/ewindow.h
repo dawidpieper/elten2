5e34c44904f5c0af59c2762c10f86cb89b74bc05
zywek123 <arekk-99@o2.pl>
2018-09-10 22:32:58 +0200

    Linux Mods

--- Client/engine/src/includes/ewindow.h
+++ Client/engine/src/includes/ewindow.h
@@ -3,5 +3,12 @@
 #include <windows.h>
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);;
 #endif
+#ifdef __linux__
+#include <X11/Xlib.h>
+Display *display;
+Window window;
+XEvent event;
+int screen;
+#endif
 int ELTEN_WINDOW_InitThr();
 #endif
