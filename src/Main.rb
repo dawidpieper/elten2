a52d57727593aaaff52ce2bb078664d183c541c9
dawidpieper <dawidpieper@o2.pl>
2020-11-10 14:35:30 +0100

    Elten 2.4 Beta 95

--- src/Main.rb
+++ src/Main.rb
@@ -10,11 +10,11 @@ end
 
 module Elten
   Version = 2.4
-  Beta = 94
+  Beta = 95
   Alpha = 0
   IsBeta = 1
   BuildID = 20201023001
-  BuildDate = 1604951557
+  BuildDate = 1605015154
   class << self
     def version
       return Version
